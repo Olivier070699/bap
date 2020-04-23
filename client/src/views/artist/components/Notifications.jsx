c8207fc03e5e8ddbd1e0ed094943069b4afafc52
Olivier Decock <olivdeco1@arteveldehs.be>
2020-04-23 08:59:48 +0200

    added artist notifications

--- client/src/views/artist/components/Notifications.jsx
+++ client/src/views/artist/components/Notifications.jsx
@@ -2,24 +2,72 @@ import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
 import '../../../style/_general.scss'
+import firebase from '../../../config/firebase'
 
 export class Notifications extends Component {
+
+    componentDidMount = () => {
+        document.querySelector('.container-notifications-content').classList.add('hide')
+        let uid = localStorage.getItem('uid')
+        firebase.database().ref('invite_artist').on('value', snap => {
+            document.querySelector('.container-notifications-content ul').innerHTML = ''
+            let amountInvitations = []
+            snap.forEach(childSnap => {
+                const data = childSnap.val()
+                if (data.artist_id === uid) {
+                    console.log(data.artist_id, uid)
+                    let content = `<li  id="${childSnap.key}">${data.agency_name} invited you to join them <button class="btn-accept" id="${data.agency_key}">accept</button> <button class="btn-remove">remove</button></li>`
+                    document.querySelector('.container-notifications-content ul').insertAdjacentHTML('beforeend', content)
+                    amountInvitations.push(data.artist_id)
+                }
+            });
+            let length = amountInvitations.length
+            document.querySelector('.amount-of-notifications').innerHTML = length
+            if (length !== 0) {
+                document.querySelector('.container-notifications-content').classList.remove('hide')
+            }
+            this.renderEventListeners()
+        })
+    }
+
+    renderEventListeners = () => {
+        let acceptBtns = document.querySelectorAll('.btn-accept')
+        acceptBtns.forEach(acceptBtn => {
+            acceptBtn.addEventListener('click', this.acceptInvitation)
+        });
+
+        let removeBtns = document.querySelectorAll('.btn-remove')
+        removeBtns.forEach(removeBtn => {
+            removeBtn.addEventListener('click', this.removeInvitation)
+        });
+    }
+
+    acceptInvitation = (e) => {
+        let artistKey = localStorage.getItem('artist_key')
+        firebase.database().ref(`artist/${artistKey}`).update({
+            agency_key: e.target.id
+        })
+        firebase.database().ref(`invite_artist/${e.target.parentNode.id}`).remove()
+        this.componentDidMount()
+    }
+
+    removeInvitation = (e) => {
+        firebase.database().ref(`invite_artist/${e.target.parentNode.id}`).remove()
+        this.componentDidMount()
+    }
+
     render() {
         return (
             <div>
                 <div className="container-notifications-icon">
-                    <div className="amount-of-notifications">1</div>
+                    <div className="amount-of-notifications"></div>
                     <FontAwesomeIcon
                         icon={faBell}
                         className="notification-icon"  
                     />
                 </div>
                 <div className="container-notifications-content">
-                    <ul>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                    </ul>
+                    <ul></ul>
                 </div>
             </div>
         )
